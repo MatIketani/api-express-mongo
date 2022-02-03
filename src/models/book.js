8844a3dd1379ea5952c09f6d51e52bf9df61d55e
MatIketani <65353228+MatIketani@users.noreply.github.com>
2022-02-03 15:55:28 -0300

    Auto-increment function added.

--- src/models/book.js
+++ src/models/book.js
@@ -1,6 +1,16 @@
 const mongoose = require('../database');
+const autoIncrement = require('mongoose-auto-increment');
+
+let db = mongoose.connection;
+
+autoIncrement.initialize(db);
 
 const bookSchema = new mongoose.Schema({
+    bookId: {
+        type: Number,
+        required: true,
+        index: true
+    },
     name: {
         type: String,
         required: true,
@@ -13,6 +23,12 @@ const bookSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     }
+});
+
+bookSchema.plugin(autoIncrement.plugin, {
+    model: 'Book', field: 'bookId',
+    startAt: 0,
+    incrementBy: 1
 });
 
 const Book = mongoose.model('Book', bookSchema);
