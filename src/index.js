8e97c39258cdbe1886987479be3790c5adf6a260
MatIketani <65353228+MatIketani@users.noreply.github.com>
2022-02-03 09:45:05 -0300

    Update on index.js, body-parser removed.
    The deprecated package body-parser removed, using express.json and express.urlencoded now.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan')
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
 require('./routes/bookRoutes')(app);
